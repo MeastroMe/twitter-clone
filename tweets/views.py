6f2fb6ca5384c999abd663da54fbd7c72fcf0015
ADvtgrHD <ibrahim.kh.90s@gmail.com>
2026-06-19 19:53:55 +0300

    Addded Login/out redirect and @login decorator

--- tweets/views.py
+++ tweets/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def index(request):
     return HttpResponse("Please work.")
 
